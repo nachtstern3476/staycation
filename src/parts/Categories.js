457cf3fd6bcf8e557c06b1b079be0d7b8f5859c6
nachtstern <yeremiawijaya2@gmail.com>
2021-09-04 11:41:17 +0700

    add mostpicked, categories part and some little changes

---
+++ src/parts/Categories.js
@@ -0,0 +1,44 @@
+import Button from 'elements/Button'
+import React from 'react'
+
+export default function Categories({data}) {
+    return data.map((category, index1)=>{
+        return (
+            <section className="container" key={`kategory-${index1}`}>
+                <h4 className="mb-3 font-weight-medium">
+                    {category.name}
+                </h4>
+                <div className="container-grid">
+                    {
+                        category.items.length === 0 ? <div className="row">
+                            <div className="col-auto align-items-center">
+                                There is no property at this category
+                            </div></div> :
+                            category.items.map((item,index2)=>{
+                                return (
+                                    <div className="item column-3 row-1" key={`category-${index1}-item-${index2}`}>
+                                        <div className="card">
+                                            {item.isPopular && (
+                                                <div className="tag">
+                                                    Popular <span className="font-weight-light">Choice</span>
+                                                </div>
+                                            )}
+                                            <figure className="img-wrapper" style={{height: 180}}>
+                                                <img src={item.imageUrl} className='img-cover' alt={item.name}/>
+                                            </figure>
+                                            <div className="meta-wrapper">
+                                                <Button className="stretched-link d-block text-gray-800" href={`/properties/${item._id}`} type="link">
+                                                    <h5 className="h4">{item.name}</h5>
+                                                </Button>
+                                                <span className="text-gray-500">{item.city}, {item.country}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                    }
+                </div>
+            </section>
+        )
+    })
+}
