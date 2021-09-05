6ea0cb3acbb2c019bf14869820288b2cc71f002d
nachtstern <yeremiawijaya2@gmail.com>
2021-09-05 13:34:04 +0700

    add reac-reveal and animation

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import TestimonyAccent from 'assets/images/testimonial-landingpages-frame.jpg'
-
+import Fade from 'react-reveal/Fade'
 import Star from 'elements/Star'
 import Button from 'elements/Button'
 
 export default function Testimony({data}) {
     return (
-        <section className="container">
-            <div className="row align-items-center">
-                <div className="col-auto" style={{marginRight: 70}}>
-                    <div className="testimonial-hero" style={{margin: `30px 0 0 30px`}}>
-                        <img src={data.imageUrl} className="position-absolute" alt="Testimonial" style={{zIndex: 2}}/>
-                        <img src={TestimonyAccent} className="position-absolute" alt="Testimonial frame" style={{margin: `-30px 0 0 -30px`, zIndex: 1}}/>
+        <Fade bottom>
+            <section className="container">
+                <div className="row align-items-center">
+                    <div className="col-auto" style={{marginRight: 70}}>
+                        <div className="testimonial-hero" style={{margin: `30px 0 0 30px`}}>
+                            <img src={data.imageUrl} className="position-absolute" alt="Testimonial" style={{zIndex: 2}}/>
+                            <img src={TestimonyAccent} className="position-absolute" alt="Testimonial frame" style={{margin: `-30px 0 0 -30px`, zIndex: 1}}/>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <h4 style={{marginBottom: 40}}>{data.name}</h4>
+                        <Star value={data.rate} width={35} height={35} spacing={4}/>
+                        <h5 className="h2 font-weight-light line-height-2 my-3">
+                            {data.content}
+                        </h5>
+                        <span className="text-gray-500">{data.familyName}, {data.familyOccupation}</span>
+                        <div>
+                            <Button className="btn px-5" style={{marginTop: 40}} hasShadow isPrimary type="link" href={`/testimonial/${data._id}`}>
+                                Read their story
+                            </Button>
+                        </div>
                     </div>
                 </div>
-                <div className="col">
-                    <h4 style={{marginBottom: 40}}>{data.name}</h4>
-                    <Star value={data.rate} width={35} height={35} spacing={4}/>
-                    <h5 className="h2 font-weight-light line-height-2 my-3">
-                        {data.content}
-                    </h5>
-                    <span className="text-gray-500">{data.familyName}, {data.familyOccupation}</span>
-                    <div>
-                        <Button className="btn px-5" style={{marginTop: 40}} hasShadow isPrimary type="link" href={`/testimonial/${data._id}`}>
-                            Read their story
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </section>
+            </section>
+        </Fade>
     )
 }
